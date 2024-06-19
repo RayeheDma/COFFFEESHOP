b9754b51f9b6de3f36166b13cd9fd3dd89f416ff
RayeheDma <rayehe1382@gmail.com>
2024-06-19 20:03:21 +0330

    Adding Reyhane's tips to keep you going

--- myproject/shop/views.py
+++ myproject/shop/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 # Display "welcome" to the web page
-def welcome_to_cafe(request):
-    return render(request, 'index.html')
+# def welcome_to_cafe(request):
+#     return render(request, 'index.html')

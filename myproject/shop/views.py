a3d3a8bf7c069dfebfbd9edfaed4f9cca180109c
RayeheDma <rayehe1382@gmail.com>
2024-06-19 20:48:39 +0330

    add bootstrap code

--- myproject/shop/views.py
+++ myproject/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 
-# Display "welcome" to the web page
-# def welcome_to_cafe(request):
-#     return render(request, 'index.html')
+
+def welcome_to_cafe(request):
+    return render(request, 'index.html')

3f27646e90a1435260deae9a98a7987c5dfa05b0
RayeheDma <rayehe1382@gmail.com>
2024-06-26 11:36:54 +0330

    login and logout

--- myproject/shop/urls.py
+++ myproject/shop/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import cafe , about
+from .views import cafe , about, login_user, logout_user
 
 urlpatterns = [
     path('', cafe, name='home'),
     path('about/',about , name = 'about'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 
 ]
+
+
